47113a45f2b01e4eb5d05e49eee7143b0ea834f4
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-04-08 18:38:38 +0600

    add tests and fixtures for task statuses

---
+++ __tests__/statuses.test.js
@@ -0,0 +1,125 @@
+// @ts-check
+
+import getApp from '../server/index.js';
+import { getTestData, prepareData } from './helpers/index.js';
+
+describe('test statuses CRUD', () => {
+  let app;
+  let knex;
+  let models;
+  const testData = getTestData();
+
+  beforeAll(async () => {
+    app = await getApp();
+    knex = app.objection.knex;
+    models = app.objection.models;
+  });
+
+  beforeEach(async () => {
+    // тесты не должны зависеть друг от друга
+    // перед каждым тестом выполняем миграции
+    // и заполняем БД тестовыми данными
+    await knex.migrate.latest();
+    await prepareData(app);
+  });
+
+  it('index', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: app.reverse('statuses'),
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('new', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: app.reverse('newStatus'),
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('create', async () => {
+    const params = testData.statuses.new;
+    const response = await app.inject({
+      method: 'POST',
+      url: app.reverse('statuses'),
+      payload: {
+        data: params,
+      },
+    });
+
+    expect(response.statusCode).toBe(302);
+    const taskStatus = await models.taskstatus.query().findOne({ name: params.name });
+    expect(taskStatus).toMatchObject(params);
+  });
+
+  it('update', async () => {
+    const responseSignIn = await app.inject({
+      method: 'POST',
+      url: app.reverse('session'),
+      payload: {
+        data: testData.users.existing,
+      },
+    });
+
+    const [sessionCookie] = responseSignIn.cookies;
+    const { name, value } = sessionCookie;
+    const cookie = { [name]: value };
+    const { id } = await models.taskstatus.query().findOne({
+      name: testData.statuses.existing.name,
+    });
+    const params = testData.statuses.new;
+
+    const responseUpdate = await app.inject({
+      method: 'PATCH',
+      url: app.reverse('editStatus', { id }),
+      cookies: cookie,
+      payload: {
+        data: params,
+      },
+    });
+
+    expect(responseUpdate.statusCode).toBe(302);
+    const updatedStatus = await models.taskstatus.query().findById(id);
+    expect(updatedStatus).toMatchObject(params);
+  });
+
+  it('delete', async () => {
+    const responseSignIn = await app.inject({
+      method: 'POST',
+      url: app.reverse('session'),
+      payload: {
+        data: testData.users.existing,
+      },
+    });
+
+    const [sessionCookie] = responseSignIn.cookies;
+    const { name, value } = sessionCookie;
+    const cookie = { [name]: value };
+    const { id } = await models.taskstatus.query().findOne({
+      name: testData.statuses.existing.name,
+    });
+
+    const responseDelete = await app.inject({
+      method: 'DELETE',
+      url: app.reverse('deleteStatus', { id }),
+      cookies: cookie,
+    });
+
+    expect(responseDelete.statusCode).toBe(302);
+    const deletedStatus = await models.taskstatus.query().findById(id);
+    expect(deletedStatus).toBeUndefined();
+  });
+
+  afterEach(async () => {
+    // после каждого теста откатываем миграции
+    await knex.migrate.rollback();
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+});
