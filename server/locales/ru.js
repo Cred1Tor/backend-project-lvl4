c6ebe17d406b801d7da4178d3867ad3f3496b935
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-04-09 12:18:26 +0600

    add locales for statuses

--- server/locales/ru.js
+++ server/locales/ru.js
@@ -27,6 +27,21 @@ module.exports = {
         notFound: 'Пользователь не найден',
         unauthorized: 'Вы не можете редактировать или удалять другого пользователя',
       },
+      statuses: {
+        create: {
+          error: 'Не удалось создать статус',
+          success: 'Статус успешно создан',
+        },
+        edit: {
+          error: 'Не удалось изменить статус',
+          success: 'Статус успешно изменен',
+        },
+        delete: {
+          error: 'Не удалось удалить статус',
+          success: 'Статус успешно удален',
+        },
+        notFound: 'Статус не найден',
+      },
       authError: 'Доступ запрещён! Пожалуйста, авторизируйтесь.',
     },
     layouts: {
@@ -35,6 +50,7 @@ module.exports = {
         signIn: 'Вход',
         signUp: 'Регистрация',
         signOut: 'Выход',
+        statuses: 'Статусы',
       },
     },
     views: {
@@ -60,6 +76,24 @@ module.exports = {
         edit: {
           submit: 'Изменить',
           editing: 'Изменение пользователя',
+        },
+      },
+      statuses: {
+        id: 'ID',
+        name: 'Наименование',
+        createdAt: 'Дата создания',
+        actions: 'Действия',
+        buttons: {
+          edit: 'Редактировать',
+          delete: 'Удалить',
+        },
+        new: {
+          submit: 'Создать',
+          creating: 'Создание статуса',
+        },
+        edit: {
+          submit: 'Изменить',
+          editing: 'Изменение статуса',
         },
       },
       welcome: {
