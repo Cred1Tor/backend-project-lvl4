a08c8d601b70540a450aaadadffcb8a8754ac431
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-05-06 23:37:11 +0600

    add Task model

---
+++ server/models/Task.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+import objectionUnique from 'objection-unique';
+import BaseModel from './BaseModel';
+
+const unique = objectionUnique({ fields: ['name'] });
+
+export default class Task extends unique(BaseModel) {
+  static get tableName() {
+    return 'tasks';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'status'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string' },
+        statusId: { type: 'integer' },
+        assignedUserId: { type: ['integer', 'null'] },
+        description: { type: 'string' },
+      },
+    };
+  }
+
+  static relationMappings: {
+    assignedUser: {
+      relation: BaseModel.BelongsToOneRelation,
+      modelClass: 'User',
+      join: {
+        from: 'tasks.assignedUserId',
+        to: 'users.id',
+      },
+    },
+    status: {
+      relation: BaseModel.BelongsToOneRelation,
+      modelClass: 'TaskStatus',
+      join: {
+        from: 'tasks.statusId',
+        to: 'statuses.id',
+      },
+    },
+  };
+}
