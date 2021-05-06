2c741bda34ae0233b6be3739d2e618f6f70411be
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-05-06 23:37:28 +0600

    add migration for creating tasks table

---
+++ server/migrations/20210420180142_create_tasks_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  knex.schema.createTable('tasks', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.string('status');
+    table.string('assignedUser');
+    table.string('description');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = (knex) => knex.schema.dropTable('tasks');
