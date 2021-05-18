7c9f5561c317be025b8ad35b31b4bc917717fa12
Aleksandr Saitgalin <cred1tor@yandex.ru>
2021-05-18 17:05:31 +0600

    expand delete test with both unauthorized and authorized cases

--- __tests__/tasks.test.js
+++ __tests__/tasks.test.js
@@ -115,7 +115,29 @@ describe('test tasks CRUD', () => {
 
     expect(responseDelete.statusCode).toBe(302);
     const deletedTask = await models.task.query().findById(id);
-    expect(deletedTask).toBeUndefined();
+    expect(deletedTask).not.toBeUndefined();
+
+    const params = testData.tasks.new;
+    await app.inject({
+      method: 'POST',
+      url: app.reverse('tasks'),
+      payload: {
+        data: params,
+      },
+      cookies: cookie,
+    });
+
+    const { id: id2 } = await models.task.query().findOne({
+      name: params.name,
+    });
+    const responseDelete2 = await app.inject({
+      method: 'DELETE',
+      url: app.reverse('deleteTask', { id: id2 }),
+      cookies: cookie,
+    });
+    expect(responseDelete2.statusCode).toBe(302);
+    const deletedTask2 = await models.task.query().findById(id2);
+    expect(deletedTask2).toBeUndefined();
   });
 
   afterEach(async () => {
